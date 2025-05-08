11dde1b037d86c66038fb37ba602617c174c83e1
Mireille05 <kelysemireille@gmail.com>
2025-05-08 17:25:54 +0200

    added codes

---
+++ loop.js
@@ -0,0 +1,18 @@
+
+
+
+function toLoop() {
+    for (let index = 0; index < 5; index++) {
+        console.log("hello");
+    } 
+}
+
+
+function ff(params) {
+    return 5;
+}
+
+ff()
+
+
+toLoop()
